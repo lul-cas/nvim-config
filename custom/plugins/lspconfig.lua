9483db3ba3b91fbe313f556daa3153cd5ce58764
wOL-Lucas <lucasefernandes333@gmail.com>
2024-02-02 13:38:52 -0300

    Create custom plugins folder

---
+++ custom/plugins/lspconfig.lua
@@ -0,0 +1,20 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- add any language you use here
+local servers = {
+  "html", "cssls", "tsserver", "clangd", "rust_analyzer", "gopls",
+  "pyright", "yamlls", "dockerls", "clojure_lsp", "cmake",
+  "terraformls", "vimls"
+}
+
+for _, lsp in ipairs(servers) do
+  local config = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+  
+  lspconfig[lsp].setup(config)
+end
