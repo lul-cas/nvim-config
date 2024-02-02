f85506b370350501f74d7f1b76c0ccc84af46fd7
wOL-Lucas <lucasefernandes333@gmail.com>
2024-02-02 11:53:52 -0300

    Added shortcuts to quit, quit and save and open terminal

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -3,4 +3,8 @@ local M = {}
 
 M.ui = { theme = 'catppuccin' }
 vim.opt.wrap = false
+
+vim.api.nvim_set_keymap('n', '<C-q>', ':q<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-s>', ':wq<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-t>', ':terminal<CR>', { noremap = true, silent = true })
 return M
