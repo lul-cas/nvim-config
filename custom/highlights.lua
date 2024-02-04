195d5e79f6c920d105cdead347747628abc9d299
wOL-Lucas <lucasefernandes333@gmail.com>
2024-02-04 13:21:04 -0300

    Create highlights file

---
+++ custom/highlights.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+---@type Base46HLGroupsList
+M.override = {
+  Comment = {
+    italic = true,
+  },
+  NvDashAscii = { bg = "NONE", fg = "vibrant_green" },
+  NvDashButtons = { bg = "NONE" },
+}
+
+return M
