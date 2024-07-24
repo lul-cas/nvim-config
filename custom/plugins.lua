39afdbedeaf02a4733cc6a66fdc252d933a47887
Lucas <lucasefernandes333@gmail.com>
2024-07-24 13:26:57 -0300

    feat(python-dapper): include plugins required by dapper

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -24,6 +24,9 @@ local plugins = {
     end
   },
   {
+    "nvim-neotest/nvim-nio"
+  },
+  {
     "mfussenegger/nvim-dap",
     config = function(_, opts)
       require("core.utils").load_mappings("dap")
